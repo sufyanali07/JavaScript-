dc0c3ffbe8620b156a4f76bac521ab33fa1b03fa
sufyanali07 <sufyan.ali6800@gmail.com>
2022-11-02 11:56:32 -0700

    Files Added

---
+++ app.js
@@ -0,0 +1,113 @@
+// ====CHAPTER 09(PROMPT):====
+// ========EXAMPLE #1========
+
+
+// var firstname = prompt("Enter Your First Name :");
+// var lastName = prompt("Enter Your last Name :");
+// var city = prompt("Enter Your city :","Karachi");
+
+// console.log(firstname);
+// console.log(lastName);
+// console.log(city);
+
+// ========EXAMPLE #2===========
+// var math     =prompt("Enter Your Math Marks :");
+// var english  =prompt("Enter Your English Marks :");
+// var urdu     =prompt("Enter Your Urdu Marks :");
+// var physics  =prompt("Enter Your Physics Marks :");
+// var islamiat =prompt("Enter Your Islamiat Marks :");
+
+// console.log(math);
+// console.log(english);
+// console.log(urdu);
+// console.log(physics);
+// console.log(islamiat);
+
+//=============================
+
+// ======CHAPTER 10(IF STATEMENT)======:
+
+// =======EXAMPLE #1=========
+
+// var officer = true  /*(Boolean true/false)*/
+
+// if(officer == true){
+//     console.log("Your document has been submitted");
+// }else{
+//     console.log("Your document has been rejected")
+//}
+
+
+//==============================
+
+
+// CHAPTER 12(IF ELSE & ELSE IF STATEMENT):
+// ========EXAMPLE #1===========
+// var age = prompt("Enter Your Age");
+// var studentCard = false;
+
+// if(age > 18){
+//     console.log("You are allowed");
+// }else if(studentCard == true){
+//     console.log("You are allowed on the basis of student cad");
+// }else{
+//     console.log("You are not allowed")
+// }
+
+
+//================================
+
+
+
+//     ========EXAMPLE #2===========
+// =======PERCENTAGE CALCULATION!=========
+
+// var math     = prompt("Enter Your Math Marks :");
+// var english  = prompt("Enter Your English Marks :");
+// var urdu     = prompt("Enter Your Urdu Marks :");
+// var physics  = prompt("Enter Your Physics Marks :");
+// var islamiat = prompt("Enter Your Islamiat Marks :");
+
+// var totalMarks = 500;
+
+// console.log(math);
+// console.log(english);
+// console.log(urdu);
+// console.log(physics);
+// console.log(islamiat);
+
+// var total = parseFloat(math) + parseFloat(english) + parseFloat(urdu) + parseFloat(physics) + parseFloat(physics) + parseFloat(islamiat);
+// // parsefloat == + (it is used to convert string values into numbers)
+// console.log(total);
+
+// var perc = total / totalMarks * 100;
+// alert(perc + "%")
+
+
+// if(perc >= 80 && perc < 100){
+//     console.log("you are in A+ division")
+// }else if(perc >= 70 && perc < 79){
+//     console.log("your are in A division");
+// }else if(perc >= 60 && perc < 69){
+//     console.log("your are in B division");
+// }else if(perc >= 50 && perc < 59){
+//     console.log("your are in C division");
+// }else if(perc >= 40 && perc < 49){
+//     console.log("your are in D division");
+// }else if(perc >= 30 && perc < 39){
+//     console.log("your are in E division");
+// }else{
+//     console.log("your are fail");
+// }
+// nested if else
+//var num = prompt("enter number");
+
+//if (num != 10) {
+//  if (num > 5) {
+//      console.log("Number is grater than 5")
+//   } else {
+//       console.log("Number is not equal to 10")
+//   }
+//}
+
+//==============================================
